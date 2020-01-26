68d4d31e40a5f38acf09f4da2f9713462112a4c8
Frank Yeats <louielouiezpc@gmail.com>
2020-01-26 20:12:37 +0800

    enter

--- 《Python编程基础》例题练习/Python数据结构小结/操作题1.py
+++ 《Python编程基础》例题练习/Python数据结构小结/操作题1.py
@@ -1,5 +1,11 @@
+#-*-coding:utf-8-*-
+
+
 list1=[5,8,-7,4,6,2,-3,0]
 print('列表的最大元素为：',max(list1))
 x=min(list1)
-y=del list1[x]
-print('最后结果为：',abs(y))
+print(x)
+y=list1.index(x)
+del list1[y]
+[abs(i)for i in list1]
+print('最后结果为：',list1)
