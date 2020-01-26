ff99af7ff296b2401e363cab9a44a405af1c4093
Frank Yeats <louielouiezpc@gmail.com>
2020-01-26 23:11:11 +0800

    enter

--- 《Python编程基础》例题练习/程序流程控制语句/for、while循环语句.py
+++ 《Python编程基础》例题练习/程序流程控制语句/for、while循环语句.py
@@ -1,2 +1,55 @@
-#-*-coding:utf-8-*-
+for是个秩序迭代器，用于遍历有序序列，包括字符串、列表、元组等
+for target_list in expression_list:
+    pass
 
+
+
+#4.3 使用for循环对列表元素和字符串进行遍历
+>>> for i in['e','f','g']:           
+...  print(i)
+...
+e
+f
+g
+>>> for i in 'string':
+...  print(i)
+...
+s
+t
+r
+i
+n
+g
+
+#4.4  while循环计数
+>>> s=0
+>>> while s<=1:
+...  print('计数：',s)
+...  s+=1
+...
+计数： 0
+计数： 1
+
+
+#4.5 无限次循环
+>>> s=1
+>>> while s<=1:
+...  print('无限次循环')
+无限次循环
+无限次循环
+无限次循环
+无限次循环
+.
+.
+.
+
+
+#4.6 range函数的使用
+
+
+
+
+
+
+
+#-*-coding:utf-8-*-
