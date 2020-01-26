73db7ea38e54a5fc0b3682178707308d360872d5
Frank Yeats <louielouiezpc@gmail.com>
2020-01-26 18:59:00 +0800

    enter

--- 《Python编程基础》例题练习/Python数据结构小结/简单的好友通讯录管理程序.py
+++ 《Python编程基础》例题练习/Python数据结构小结/简单的好友通讯录管理程序.py
@@ -1,7 +1,25 @@
 #-*-coding:utf-8-*-
 
-dict1={'小明':'001':'广州','小红':'002':'深圳','小王':'003':'北京'}            #
-x=input('输入数字1进行好友添加，输入数字2删除好友，输入数字3进行好友信息修,输入数字4进行好友信息查询')
+dict1={'小明':'广州','小红':'深圳','小王':'北京'} 
+x=input('输入数字1进行好友添加，输入数字2删除好友，输入数字3进行好友信息修改,输入数字4进行好友信息查询')
 x=int(x)
-if 
-dict1.update()
+if x=1
+    dict_1=input('请输入姓名：')
+    dict_2=input('请输入电话：')
+    dict_3=input('请输入地址：')
+    dict2=dict_1,dict_2,dict_3
+    dict1.update(dict2)
+else:
+    if x=2
+    del_name=input('请输入要删除好友的姓名：')
+    del dict1[del_name]
+else:
+    if x=3
+    alter_name=input('请输入要修改好友的姓名：')
+    alter_name1=input('输入更改姓名：')
+    alter_address1=input('请输入更改地址：')
+    dict[alter_name]='alter_name1','alter_address1'
+else:
+    if x=4
+    ask_name=input('请输入要查询好友的姓名：')
+    dict1.get('ask_name')
