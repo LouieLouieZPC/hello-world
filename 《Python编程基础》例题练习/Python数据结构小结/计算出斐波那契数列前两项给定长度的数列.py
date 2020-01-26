26a7c7e5ba62aaf7baf27f14084968ab70eb9e51
Frank Yeats <louielouiezpc@gmail.com>
2020-01-26 16:23:26 +0800

    enter

---
+++ 《Python编程基础》例题练习/Python数据结构小结/计算出斐波那契数列前两项给定长度的数列.py
@@ -0,0 +1,14 @@
+#-*-coding:utf-8-*-
+F1=0
+F2=1
+list1=[F1,F2]
+F3=F1+F2;list1.append(F3)
+F4=F2+F3;list1.append(F4)
+F5=F3+F4;list1.append(F5)
+F6=F4+F5;list1.append(F6)
+list1.pop(2)
+print('此指定长度为5的数列为：',list1)
+F6=sum(list1)
+print('此时数列各项之和为:',F6)
+list1.append(F6)
+print('追加新项的数列为：',list1)
