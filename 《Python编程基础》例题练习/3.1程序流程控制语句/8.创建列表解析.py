0d0931ce69939da2d22320296d5afec760641790
Frank Yeats <louielouiezpc@gmail.com>
2020-01-28 15:07:34 +0800

    enter

--- 《Python编程基础》例题练习/3.1程序流程控制语句/8.创建列表解析.py
+++ 《Python编程基础》例题练习/3.1程序流程控制语句/8.创建列表解析.py
@@ -7,9 +7,21 @@
 [1, 3, 5, 7]
 
 # 列表解析式嵌套循环示例
+
+# 例一，法一：
 >>> [(i,j) for i in range(0,3) for j in range(0,3)]    # (打印 迭代 嵌套迭代)
 [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
-
+# 例一，法二：
+for i in range(0,3):                                   # 法二与法一所得效果相同
+    for j in range(0,3):
+        print((i,j),end=',') 
+# 例二，法一：
 >>> [(i,j) for i in range(0,3) if i<1 for j in range(0,3) if j>1]   # (打印 迭代 满足的条件 嵌套迭代 满足的条件)
 [(0, 2)]
+# 例二，法二：
+for i in range(0,3):                                                # 法二与法一所得效果相同
+    if i<1:
+        for j in range(0,3):
+            if j>1:
+                print([(i,j)])
 
