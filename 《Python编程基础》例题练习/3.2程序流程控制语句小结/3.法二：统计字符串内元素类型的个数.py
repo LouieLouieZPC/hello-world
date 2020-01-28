833c8721dfc27efa7d5d66f883ebdedc666c36e1
Frank Yeats <louielouiezpc@gmail.com>
2020-01-28 22:54:41 +0800

    enter

--- 《Python编程基础》例题练习/3.2程序流程控制语句小结/3.法二：统计字符串内元素类型的个数.py
+++ 《Python编程基础》例题练习/3.2程序流程控制语句小结/3.法二：统计字符串内元素类型的个数.py
@@ -21,13 +21,13 @@ def strnum(element):     # 定义一个函数，
     intCount=0
     strCount=0
     otherCount=0
-    for i in element:
+    for i in element:    # for循环语句
         if i.isdigit():
             intCount+=1
         elif i.isalpha():
             strCount+=1
         else:
             otherCount+=1
-    return(intCount,strCount,otherCount)
+    return[intCount,strCount,otherCount]
 element=input('请输入一段字符串：')
 print(strnum(element))                   # 调用函数
