7a92742d491be9da390e2e22d23ff6fa99a46a3b
Frank Yeats <louielouiezpc@gmail.com>
2020-01-28 15:26:26 +0800

    enter

--- 《Python编程基础》例题练习/3.1程序流程控制语句/8.创建列表解析.py
+++ 《Python编程基础》例题练习/3.1程序流程控制语句/8.创建列表解析.py
@@ -3,11 +3,12 @@
 a=list(map(lambda x:x**3,range(6)))  # map函数根据提供的函数对指定序列做映射(lambda匿名函数 打印:程序段，一个或多个序列)
 print(a)
 # 例1，法二：
->>> [x**3 for x in range(6)]         # (打印 迭代)计算x的3次迷
+>>> [x**3 for x in range(6)]         # (打印 迭代)计算x的3次幂
 [0, 1, 8, 27, 64, 125]
+
 # 例2，法一：
 seq=[1,2,3,4,5,6,7,8]
-a=list(filter(lambda x:x%2,seq))# filter用于过滤序列，过滤不符合条件的元素，返回符合条件元素组成新列表(lambda匿名函数 打印:条件，可迭代对象)
+a=list(filter(lambda x:x%2,seq))     # filter用于过滤序列，过滤不符合条件的元素，返回符合条件元素组成新列表(lambda匿名函数 打印:条件，可迭代对象)
 print(a)
 # 例2，法二：
 >>> seq=[1,2,3,4,5,6,7,8]
