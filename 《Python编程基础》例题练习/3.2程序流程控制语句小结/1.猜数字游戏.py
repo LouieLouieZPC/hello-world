ab030f62728f648eaf29a42c7b58d3e4b171ea26
Frank Yeats <louielouiezpc@gmail.com>
2020-01-28 19:35:33 +0800

    enter

--- 《Python编程基础》例题练习/3.2程序流程控制语句小结/1.猜数字游戏.py
+++ 《Python编程基础》例题练习/3.2程序流程控制语句小结/1.猜数字游戏.py
@@ -1,25 +1,24 @@
 #-*-coding:utf-8-*-
 
 import random
-x=int(random.randint(0,500))
+x=random.randint(0,500)
 print('《猜数字游戏》')
 num=int(input('电脑已随机生成一个0~500的整数，请输入一个数字看看是否与电脑给的一样:'))
 guess=0
-while type(num) is int:
-    while num!=x:
-        if int(num)>int(x):   
-            guess+=1
-            print('你第%d次输入的数字%d比电脑的随机数字大'%(guess,num))
-            num=input('请再次输入数字：')
-            continue
-        elif int(num)>int(x):         
-            guess+=1
-            print('你第%d次输入的数字%d比电脑的随机数字小'%(guess,num))
-            num=input('请再次输入数字：')
-            continue
+while (0<=num and num<=500):
+    if num>x:
+        guess+=1
+        print('你第%d次输入的数字%d比电脑的随机数字大'%(guess,num))
+        num=int(input('请再次输入数字：'))
+    elif num<x:         
+        guess+=1
+        print('你第%d次输入的数字%d比电脑的随机数字小'%(guess,num))
+        num=int(input('请再次输入数字：'))
     else:
         guess+=1
-        print('恭喜您，您第%d次输入的数字%d与电脑的随机数字%d一样'%(guess,num,x))
+        print('恭喜您，正确答案是：%d，您第%d次输入时猜对了'%(x,guess))
+        break
+    continue
 else:
     print('您所输入的不符合规范，请重新输入！')
     num=int(input('请重新输入符合规范的数字：'))
