2790dc467b172554911594d2d3d50ffb788a5627
Frank Yeats <louielouiezpc@gmail.com>
2020-01-29 19:25:41 +0800

    enter

--- 《Python编程基础》例题练习/4.1函数/1.自定义函数.py
+++ 《Python编程基础》例题练习/4.1函数/1.自定义函数.py
@@ -16,8 +16,87 @@
 0.684931506849315
 >>> interest(10000)
 1.36986301369863
-# 5.4 任意数量的位置可变参数
 
+
+
+# 5.4 任意数量的位置可变参数
+'''
+*和**
+实际上真正的Python参数传递语法是*和**。
+*args和**kwargs只是一种约定俗成的编程实践。我们也可以写成*vars和**kvars。
+
+*args和**kwargs一般是用在函数定义的时候。二者的意义是允许定义的函数接受任意数目的参数。
+也就是说我们在函数被调用前并不知道也不限制将来函数可以接收的参数数量。
+
+*args用来表示函数接收可变长度的非关键字参数列表作为函数的输入，存储在元组中。
+**kwargs表示函数接收可变长度的关键字参数字典作为函数的输入。
+
+'''
+>>> def exp(x,y,*args):         # 定义函数
+...  print('x:',x)
+...  print('y:',y)
+...  print('args:',args)
+...
+>>> exp(1,5,66,55,'abc')
+x: 1
+y: 5
+args: (66, 55, 'abc')           # *args参数传入函数后存储在元组中
+
+
+
+# 5.5 任意数量的关键字可变参数
+>>> def exp(x,y,*args,**kwargs):
+...  print('x:',x)
+...  print('y:',y)
+...  print('args:',args)
+...  print('kwargs:',kwargs)
+...
+>>> exp(1,2,2,4,6,a='c',b=1)
+x: 1
+y: 2
+args: (2, 4, 6)
+kwargs: {'a': 'c', 'b': 1}
+
+
+
+# 5.6 return函数
+>>> def interest_r(money,day=1,interest_rate=0.05):
+...  income=0
+...  income=money*interest_rate*day/365
+...  return income
+
+# 5.7print和return的区别
+>>> x=interest(1000)        
+0.136986301369863
+>>> x                   # print函数仅仅是直接打印了对象，打印出来的结果没有被保存在x里，无法调用
+
+>>> y=interest_r(1000)
+>>> y
+0.136986301369863       # print函数返回的结果保存在y中，可供其他函数调用
+
+
+
+# 5.8 传入位置参数
+>>> list(range(0,10,2)) # 按start=0,stop=10,step=2的顺序传入
+[0, 2, 4, 6, 8]
+>>> list(range(10,0,2)) # 调转start和stop的顺序后传入
+[]
+>>> list(range(10,2,0)) # 调转全部参数的顺序后报错（位置参数顺序不可调换）
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ValueError: range() arg 3 must not be zero
+
+
+
+# 5.9 调用位置参数
+>>> list(range(0,10,1))
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> list(range(10))             # 当函数的参数有默认设置值时，可以不设置该函数会自动用默认值
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+# 5.10 设置关键字参数
 
 
 
