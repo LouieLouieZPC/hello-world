57664abb0ac8073167a3f71f917c9f96b8f321c1
Frank Yeats <louielouiezpc@gmail.com>
2020-01-30 16:31:54 +0800

    enter

--- 《Python编程基础》例题练习/4.1函数/5.lambda函数、map函数、fib函数、filter函数.py
+++ 《Python编程基础》例题练习/4.1函数/5.lambda函数、map函数、fib函数、filter函数.py
@@ -4,3 +4,13 @@
 <function <lambda> at 0x00000242938D0160>
 >>> example(2)
 8
+
+
+# 5.23使用lambda语句创建对数函数
+>>> from math import log                    # 引如math数学库的对数函数
+>>> def make_logarithmic_function(base):    # 此函数用于返回一个以base为底的匿名对数函数
+...  return lambda x:log(x,base)            # 返回一个以base为底，以x为真数的匿名对数函数
+...
+>>> my_log=make_logarithmic_function(3)     # （python允许将lambda语句作为对象赋值给变量，然后使用变量名进行调用）调用匿名函数my_log，底数已经设置为默认参数3，x为位置参数)
+>>> print(my_log(9))                        # 
+2.0
