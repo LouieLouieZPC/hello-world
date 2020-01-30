8d925f3c52f60211f9bee88da0dd944197a51d3f
Frank Yeats <louielouiezpc@gmail.com>
2020-01-30 22:01:36 +0800

    enter

--- 《Python编程基础》例题练习/4.1函数/7.存储并导入streak模块.py
+++ 《Python编程基础》例题练习/4.1函数/7.存储并导入streak模块.py
@@ -1,9 +1,19 @@
 # 法一：
-import steak                 # 导入模块
+import steak                 # import 模块
 steak.make_steak(9,'salad') # 模块名前缀.函数()
-Make a steak well done in 9with the other:
+'''
+运行结果如下：
+Make a steak well done in 9 with the other:
 ———salad
+'''
 # 法一变种：指定模块别名
+import steak as S          # import 模块 as 别名
+S.make_steak(9,'red wine','salad')  # 模块别名前缀.函数()
+'''
+运行结果如下：
+Make a steak well done in 9 with the other:
+———red wine
+———salad
+'''
+# 法二
 
-
-#  
