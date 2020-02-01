94cc599a05ae06b7943e5b63b66638e656335286
Frank Yeats <louielouiezpc@gmail.com>
2020-02-01 16:13:02 +0800

    enter

--- 《Python编程基础》例题练习/5.1面对对象编程/7.Task2（创建Car对象）.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/7.Task2（创建Car对象）.py
@@ -14,7 +14,8 @@ class Car:
         print('---解析方法被调用---')
 BMW=Car(4,'red')
 print(BMW)
-print('车的颜色为：',self.newColor)   # 此类的数据属性与方法还未私有化，以此可以访问。
-print(BMW.run())     # 调用对象的run函数
+print('车的颜色为：',   BMW.newColor)   # 此类的数据属性与方法还未私有化，以此可以访问。访问对象的属性时，后面不用加括号
+print('车轮子的数量为：',BMW.newWheelNum) # 访问对象的属性时，后面不用加括号
+print(BMW.run())     # 调用对象的run函数/方法，访问对象的方法时，后面需要加括号()，因为是有参数的
 del BMW              # 删除对象
 print(BMW)           # 查看对象是否被删除
