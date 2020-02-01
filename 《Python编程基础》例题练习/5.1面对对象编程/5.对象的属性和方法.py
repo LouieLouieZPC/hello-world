9dd512b58e3ee36e1bb569b0735b77cd2113b7e2
Frank Yeats <louielouiezpc@gmail.com>
2020-02-01 14:31:04 +0800

    enter

--- 《Python编程基础》例题练习/5.1面对对象编程/5.对象的属性和方法.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/5.对象的属性和方法.py
@@ -1,15 +1,37 @@
+# 6.7创建实例
 class Cat():
     def __init__(self,name,age):
         self.name=name
         self.age=age
-    def _sleep_(self):
-        print('%d岁的%s正在沙发上睡懒觉。'%(self.age,))
-    def _eat_(self,food):
+    def sleep(self):
+        print('%d岁的%s正在沙发上睡懒觉。'%(self.age,self.name))
+    def eat(self,food):
         self.food=food
         print('%d岁的%s正在吃%s。'%(self.age,self.name,self.food))
-cat1=Cat('Tom',3)                 # 创建对象cat1
-cat2=Cat('Jack',4)                # 创建对象cat1
-print('Cat1的名字为：',cat1.name) 
+cat1=Cat('Tom',3)                 # 创建实例/对象cat1需要参数
+cat2=Cat('Jack',4)                # 创建实例/对象cat1需要参数
+print('Cat1的名字为：',cat1.name)  # 调用函数
+print('Cat2的名字为：',cat2.name)  # 调用函数
+print(cat1.sleep())               # 访问对象的方法/函数sleep，记得加括号
+print(cat2.eat('fish'))           # 访问对象的方法/函数eat，记得加括号
+
+'''
+上述结果如下：
 Cat1的名字为： Tom
-print('Cat2的名字为：',cat2.name)
 Cat2的名字为： Jack
+3岁的Tom正在沙发上睡懒觉。
+None
+4岁的Jack正在吃fish。
+None
+'''
+
+
+# 6.8对象方法的引进
+cat1=Cat('Tom',3)
+sleep=cat1.sleep      # 对象的方法/函数sleep的引用
+print(sleep())        # 记得加括号
+
+cat1=Cat('Tom',3)
+eat=cat1.eat     # 对象的方法/函数eat的引用
+print(eat())     # 记得加括号
+
