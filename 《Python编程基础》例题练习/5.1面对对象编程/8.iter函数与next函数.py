45d0028ec646c096421aa3ee79f005c9e4326655
Frank Yeats <louielouiezpc@gmail.com>
2020-02-01 19:12:38 +0800

    enter

--- 《Python编程基础》例题练习/5.1面对对象编程/8.iter函数与next函数.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/8.iter函数与next函数.py
@@ -1,9 +1,9 @@
 # 6.13 迭代器（iteration）的两个基本函数：iter函数与next函数
->>> L=[1,2,3]
->>> it=iter(L)        # 调用iter函数
+>>> L=[1,2,3]         # 字符串，列表或元组对象都可用于创建迭代器
+>>> it=iter(L)        # 调用iter函数创建迭代器对象，变量=iter()
 >>> it             
 <list_iterator object at 0x0000023B1BCEFFD0>
->>> next(it)
+>>> next(it)          # 输出迭代器的下一个元素，next()
 1
 >>> next(it)
 2
@@ -13,3 +13,14 @@
 Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
 StopIteration        # 停止迭代
+
+
+# 迭代器对象可以使用常规for语句进行遍历、
+list=[1,2,3,4]
+it = iter(list)    # 创建迭代器对象
+for x in it:
+    print (x, end=" ")
+'''
+结果如下：
+1 2 3 4
+'''
--- 《Python编程基础》例题练习/5.1面对对象编程/9.给类加入迭代器.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/9.给类加入迭代器.py
@@ -1,5 +1,8 @@
-# 9.给类加入迭代器,需定义_iter_函数
-
+# 9.给类加入迭代器
+'''
+把一个类作为一个迭代器使用需要在类中实现两个方法 __iter__() 与 __next__() 。
+'''
+# 例一、
 class  Cat():
     def __init__(self,name,age):
         self.name=name
@@ -31,4 +34,39 @@ for info in iterator:
 结果如下：
 coffe
 3
+'''
+
+
+
+# 例二、
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self         # __iter__() 方法返回一个特殊的迭代器对象
+ 
+  def __next__(self):
+    x = self.a
+    self.a += 1         # 初始值为 1，逐步递增 1
+    return x
+ 
+myclass = MyNumbers()
+myiter = iter(myclass)
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+'''
+以上结果如下：
+>>> print(next(myiter))
+1
+>>> print(next(myiter))
+2
+>>> print(next(myiter))
+3
+>>> print(next(myiter))
+4
+>>> print(next(myiter))
+5
 '''
