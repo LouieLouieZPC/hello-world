48a024cc00bfae45a114b8bf3823b1ba6ed47698
Frank Yeats <louielouiezpc@gmail.com>
2020-02-01 19:28:41 +0800

    enter

--- 《Python编程基础》例题练习/5.1面对对象编程/9.给类加入迭代器.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/9.给类加入迭代器.py
@@ -15,19 +15,19 @@ class  Cat():
         return self.age
     def _iter_(self):         # 加入迭代器
         print('名字 年龄')
-        return self
+        return self           # __iter__() 方法返回一个特殊的迭代器对象
     def next(self):
         if self.index==len(self.info)-1:
             raise StopAsyncIteration
         self.index+=1
         return self.info[self.index]
-newcat=Cat('coffe',3)         # 创建对象
+newcat=Cat('coffe',3)         # 创建类的对象
 print(newcat.getName())       # 通过设置好的接口函数来访问对象的属性
 '''
 结果如下：
 coffe
 '''
-iterator=iter(newcat.next,1) # 调用迭代函数来输出对象的属性
+iterator=iter(newcat.next,1) # 创建迭代器对象，变量=iter(类的对象)，调用迭代函数来输出对象的属性
 for info in iterator:
     print(info)
 '''
@@ -50,7 +50,7 @@ class MyNumbers:
     return x
  
 myclass = MyNumbers()   # 创建类的对象
-myiter = iter(myclass)  # 创建迭代器对象，变量=iter()
+myiter = iter(myclass)  # 创建迭代器对象，变量=iter(类的对象)
 
 print(next(myiter))     # 输出迭代器的下一个元素，next()
 print(next(myiter))
@@ -86,10 +86,15 @@ class MyNumbers:
       self.a += 1
       return x
     else:
-      raise StopIteration
+      raise StopIteration    # 在 20 次迭代后停止执行
  
-myclass = MyNumbers()
-myiter = iter(myclass)
+myclass = MyNumbers()        # 创建类的对象
+myiter = iter(myclass)       # 创建迭代器对象，变量=iter(类的对象)
  
 for x in myiter:
-  print(x)
+  print(x,end=',')
+
+'''
+以上结果如下：
+1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+'''
