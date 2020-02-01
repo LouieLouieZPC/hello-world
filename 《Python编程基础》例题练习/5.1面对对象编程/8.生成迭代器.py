36613226f3aa1fa98c6eb52484463ba9b2ceb1a7
Frank Yeats <louielouiezpc@gmail.com>
2020-02-01 16:55:59 +0800

    enter

--- 《Python编程基础》例题练习/5.1面对对象编程/8.生成迭代器.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/8.生成迭代器.py
@@ -1,6 +1,7 @@
+# 6.13 迭代器（iteration）的两个基本函数：it函数与next函数
 >>> L=[1,2,3]
->>> it=iter(L)
->>> it
+>>> it=iter(L)        # 调用iter函数
+>>> it             
 <list_iterator object at 0x0000023B1BCEFFD0>
 >>> next(it)
 1
@@ -8,7 +9,9 @@
 2
 >>> next(it)
 3
->>> next(it)
+>>> next(it)         #容器对象遍历/迭代完毕，next函数找不到后续元素引发StopIteration异常，告知for循环终止
 Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
-StopIteration
+StopIteration        # 停止迭代
+
+# 
