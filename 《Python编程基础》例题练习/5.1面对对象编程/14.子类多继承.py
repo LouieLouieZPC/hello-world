48039b19651fefc8c1fc284e76a632156fd12386
Frank Yeats <louielouiezpc@gmail.com>
2020-02-02 16:31:22 +0800

    enter

--- 《Python编程基础》例题练习/5.1面对对象编程/14.子类多继承.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/14.子类多继承.py
@@ -18,3 +18,22 @@ class D(B,C):
         B.__init__(self)
         C.__init__(self)
         print('  <-Output D')
+d=D()
+print(issubclass(C,B))        # issubclass() 方法的语法:issubclass(class, classinfo).如果 class 是 classinfo 的子类返回 True，否则返回 False。
+print(issubclass(C,A))
+
+'''
+以上结果如上：
+  ->Input D
+  ->Input B
+  ->Input A
+  <-Output A
+  <-Output B
+  ->Input C
+  ->Input A
+  <-Output A
+  <-Output C
+  <-Output D
+False
+True
+'''
