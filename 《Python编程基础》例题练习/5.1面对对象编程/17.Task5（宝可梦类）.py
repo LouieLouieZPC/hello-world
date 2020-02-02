564ba1d5231c9293ecb7231abc2b281812fb3ca0
Frank Yeats <louielouiezpc@gmail.com>
2020-02-02 21:31:03 +0800

    enter

---
+++ 《Python编程基础》例题练习/5.1面对对象编程/17.Task5（宝可梦类）.py
@@ -0,0 +1,16 @@
+class Charmander(object):
+    def __init__(self,name,gender,level,status,type)
+    self.name=name
+    self.gender=gender
+    self.level=level
+    self.status=status
+    self.type=type
+    self.info=[self,self.name,self.gender,self.level,self.status,self.type]
+    self.index=-1
+    def getName(self):
+        return self.name
+    def getGender(self):
+        return self.gender
+    def getStatus(self):
+        return self.status
+        
