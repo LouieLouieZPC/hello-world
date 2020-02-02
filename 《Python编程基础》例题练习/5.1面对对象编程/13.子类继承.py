bcdc06d07291e302bd6c3d013d9d3a96bbbe184a
Frank Yeats <louielouiezpc@gmail.com>
2020-02-02 14:59:30 +0800

    enter

--- 《Python编程基础》例题练习/5.1面对对象编程/13.子类继承.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/13.子类继承.py
@@ -11,7 +11,24 @@ class Cat():
     def getAge(self):
         return self.age
     def _iter_(self):
+        print('名字 年龄')
+        return self
+    def next(self):
         if self.index==len(self.info)-1:
             raise StopIteration
         self.index+=1
         return self.info[self.index]
+class Bosi(Cat):
+    def setName(self,newName):
+        self.name=newName
+    def eat(self):
+        print(self.name,'--is eating')
+bs=Bosi()
+print('bs的名字是：',bs.name)
+print('bs的年龄为：',bs.age)
+print(bs.run())
+bs.setName('波斯猫')
+bs.eat()
+iteration=iter(bs.next,1)
+for info in iteration:
+    print(info)
