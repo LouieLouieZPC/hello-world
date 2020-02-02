e1781c878238f19d5fce010e7e8a4afad944dad6
Frank Yeats <louielouiezpc@gmail.com>
2020-02-02 14:24:42 +0800

    enter

--- 《Python编程基础》例题练习/5.1面对对象编程/12.Task3（迭代Car对象）.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/12.Task3（迭代Car对象）.py
@@ -8,7 +8,7 @@ class Car:
         self.Color=Color                    # 属性                       
         self.T=T                                  # 属性
         self.info=[self.brand,self.WheelNum,self.Color,self.T]
-        self.index=-1
+        self.index=-1                       # 为迭代设置初始变量
     def getBrand(self):                            # 方法(函数)
         return self.brand
     def getWheelNum(self):                         # 方法(函数)
@@ -20,7 +20,7 @@ class Car:
     def _iter_(self):                               # 方法(函数)
         print('品牌 车辆数 颜色 废气涡轮增压')
         return self
-    def next(self):
+    def next(self):                                # 方法(函数)
         if self.index==3:
             raise StopIteration
         self.index+=1
@@ -30,4 +30,14 @@ newcar=Car('BMW',4,'red',2.4)                  # 创建对象，输入参数
 print('车的颜色为：',newcar.Color)              # 此类的数据属性与方法还未私有化，以此可以访问。访问对象的属性时，后面不用加括号
 iterator=iter(newcar.next,1)                   # iter(object, sentinel)创建迭代器对象，调用迭代该函数输出对象的属性（如果传递了第二个参数，则object必须是一个可调用的对象（如，函数）。每次调用这个迭代器对象的__next__()方法时，都会调用object。）
 for info in iterator:                          # 遍历迭代器对象的同时每次都会调用next函数
-    print(info)
+    print(info)                                # 输出每次的遍历结果
+
+
+'''
+以上结果如下：
+车的颜色为： red
+BMW
+4
+red
+2.4
+'''
