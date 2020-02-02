d6675f4dada3c6a2acdf5f91a14dc64ab6d41a81
Frank Yeats <louielouiezpc@gmail.com>
2020-02-02 21:41:43 +0800

    enter

--- 《Python编程基础》例题练习/5.1面对对象编程/17.Task5（宝可梦类）.py
+++ 《Python编程基础》例题练习/5.1面对对象编程/17.Task5（宝可梦类）.py
@@ -5,7 +5,7 @@ class Charmander(object):
     self.level=level
     self.status=status
     self.type=type
-    self.info=[self,self.name,self.gender,self.level,self.status,self.type]
+    self.info=[self,self.name,self.type,self.gender,self.level,self.status]
     self.index=-1
     def getName(self):
         return self.name
@@ -13,4 +13,14 @@ class Charmander(object):
         return self.gender
     def getStatus(self):
         return self.status
-        
+    def getType(self):
+        return self.type
+    def __iter__(self):
+        print('名字 属性 性别 等级 能力')
+        return self
+    def next(self):
+        if self.index==4:
+            raise StopIteration
+        self.index+=1
+        return self.info[self.index]
+pokemon1=
