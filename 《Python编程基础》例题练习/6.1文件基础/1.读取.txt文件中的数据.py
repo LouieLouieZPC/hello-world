74749bebeb8f37f9eb48506b970d81c0f3fa6838
Frank Yeats <louielouiezpc@gmail.com>
2020-02-03 15:00:43 +0800

    enter

--- 《Python编程基础》例题练习/6.1文件基础/1.读取.txt文件中的数据.py
+++ 《Python编程基础》例题练习/6.1文件基础/1.读取.txt文件中的数据.py
@@ -1,3 +1,11 @@
-f=open(r'D:\01.Software\GitHub\GitHub Repository\hello-world\《Python编程基础》例题练习\6.1文件基础\e_point.txt','r')
+
+# 打开并读取文件
+f=open(r'D:\01.Software\GitHub\GitHub Repository\hello-world\《Python编程基础》例题练习\6.1文件基础\e_point.txt','r')     # 只读文件，通过绝对路径打开文件并定义变量f
 txt=f.read()
 print(txt)
+'''
+以上结果如下：
+2.7182818284
+4904523536
+0287471352
+'''
