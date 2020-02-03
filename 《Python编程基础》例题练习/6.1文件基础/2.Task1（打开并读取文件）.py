062617656b6d0bebe871f70ad0bc757d9a72d6c0
Frank Yeats <louielouiezpc@gmail.com>
2020-02-03 17:12:57 +0800

    enter

--- 《Python编程基础》例题练习/6.1文件基础/2.Task1（打开并读取文件）.py
+++ 《Python编程基础》例题练习/6.1文件基础/2.Task1（打开并读取文件）.py
@@ -1,4 +1,4 @@
 txts=r'D:\01.Software\GitHub\GitHub Repository\hello-world\《Python编程基础》例题练习\6.1文件基础\Walden.txt'
-with open(txts,'r') as f:
+with open(txts,'r',encoding='UTF-8') as f:          # 再加一个编码方式encoding='UTF-8'
     x=f.read()
 print(x)
