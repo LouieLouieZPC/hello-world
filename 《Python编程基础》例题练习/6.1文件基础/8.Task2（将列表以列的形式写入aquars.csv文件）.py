754c427c288da84c7b2723517c0e91d05223fb5a
Frank Yeats <louielouiezpc@gmail.com>
2020-02-04 12:23:57 +0800

    enter

--- 《Python编程基础》例题练习/6.1文件基础/8.Task2（将列表以列的形式写入aquars.csv文件）.py
+++ 《Python编程基础》例题练习/6.1文件基础/8.Task2（将列表以列的形式写入aquars.csv文件）.py
@@ -1,4 +1,7 @@
 import csv
 squares=[value**2 for value in range(1,101)]
 file_name=r'D:\01.Software\GitHub\GitHub Repository\hello-world\《Python编程基础》例题练习\\6.1文件基础\squares.py'
-with open()
+with open(file_name,'w',newline='') as f:
+    x=csv.writer(f)
+    for i in squares:
+        x.writerow([str(i)])    # 转换成列表中的字符串
