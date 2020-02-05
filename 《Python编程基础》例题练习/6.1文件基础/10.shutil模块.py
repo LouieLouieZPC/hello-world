38f3077d09a0035f6787053828fb69fb90a58d41
Frank Yeats <louielouiezpc@gmail.com>
2020-02-05 16:20:50 +0800

    enter

--- 《Python编程基础》例题练习/6.1文件基础/10.shutil模块.py
+++ 《Python编程基础》例题练习/6.1文件基础/10.shutil模块.py
@@ -44,4 +44,38 @@ import shutil
 os.unlink(r'D:\01.Software\GitHub\GitHub Repository\hello-world\《Python编程基础》例题练习\6.1文件基础\test2')
 os.rmdir(r'D:\01.Software\GitHub\GitHub Repository\hello-world\《Python编程基础》例题练习\6.1文件基础\test')
 shutil.rmtree(r'D:\01.Software\GitHub\GitHub Repository\hello-world\《Python编程基础》例题练习\7.1Test目录')
-# 
+
+
+'''
+1.压缩(将文件夹压缩到文件中)：
+shutil.make_archive(base_name,format,root_dir,owner,group,logger)函数：
+
+base_name： 压缩包的文件名，也可以是压缩包的路径。如果不指定绝对路径，只是文件名时，则保存至当前目录，否则保存至指定路径，路径指向文件名！！！
+                        如：www                        =>保存至当前路径
+                        如：/Users/aaa/www =>保存至/Users/aaa/
+format  ：   压缩包种类，“zip”, “tar”, “bztar”，“gztar”,“xztar”
+root_dir：   要压缩的文件夹路径（默认当前目录）,路径指向文件夹！！！！！
+owner   ：   用户，默认当前用户
+group   ：   组，默认当前组
+logger  ：   用于记录日志，通常是logging.Logger对象
+'''
+# 压缩文件
+import shutil
+shutil.make_archive(r'C:\Users\66435\OneDrive\桌面\Iris数据集\哈哈哈','zip',r'C:\Users\66435\OneDrive\桌面\Iris数据集')
+
+
+
+
+
+'''
+ 2.解压（将压缩包解压到文件夹中）：
+ shutil.unpack_archive(filename[, extract_dir[, format]])函数：
+ 解压缩或解包源文件。
+filename是压缩文档的完整路径，路径指向文件名，加格式后缀！！！
+extract_dir是解压缩路径，默认为当前目录，路径指向文件夹！！！
+format是压缩格式。默认使用文件后缀名代码的压缩格式。
+'''
+# 解压文件
+import shutil
+shutil.unpack_archive(r'C:\Users\66435\OneDrive\桌面\Iris数据集\Titanic.zip',
+r'C:\Users\66435\OneDrive\桌面\压缩包文件夹')
