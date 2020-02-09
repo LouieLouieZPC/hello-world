2291e2aa3776cd6854627001b875eb609f7b0905
Frank Yeats <louielouiezpc@gmail.com>
2020-02-09 20:29:03 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,4 +1,21 @@
-L=[('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
-L2 = sorted(L, key=lambda L:L[0])
-print(L2)
-[('Adam', 92), ('Bart', 66), ('Bob', 75), ('Lisa', 88)]
+def count():
+    fs = []
+    for i in range(1, 4):     # 循环变量i
+        def f():
+             return i*i       # 此处引用了循环变量
+        fs.append(f)
+    return fs
+
+
+def count():
+    def n(x):
+        def n1():
+            return x*x
+        return n1
+    n2=[]
+    for i in range(1,4):
+        n2.append(n(i)) 
+    return n2
+
+f1, f2, f3 = count()
+print(f1(),f2(),f3())
