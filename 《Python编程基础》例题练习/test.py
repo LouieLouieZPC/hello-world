4ae8ddfc963561daf6aae737e4b20a87dfde6a8e
Frank Yeats <louielouiezpc@gmail.com>
2020-02-09 15:13:12 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -10,5 +10,5 @@ print(reduce(lambda x,y: x*y,[3, 5, 7, 9]))
 945
 
 from functools import reduce
-digits={'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'0':0}
-        reduce(,reduce(lambda x:digits[n],n))
+digits={'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'0':0,'.':.}
+print(reduce(lambda x,y: x*10+y,map(lambda n:digits[n],'123.456')))
