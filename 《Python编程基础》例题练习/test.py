233b70e167bacb1ddc0d2d982558fc5f0f304f11
Frank Yeats <louielouiezpc@gmail.com>
2020-02-09 21:48:34 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,15 +1,2 @@
-def createCounter():
-    num=[0]
-    def counter():
-        num[0]+=1
-        return num[0]
-    return counter
-
-# 测试:
-counterA = createCounter()
-print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
-counterB = createCounter()
-if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
-    print('测试通过!')
-else:
-    print('测试失败!')
+L=list(filter(lambda n: n%2==1,range(1,20)))
+print(L)
