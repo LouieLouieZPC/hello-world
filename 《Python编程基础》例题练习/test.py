6dc17a28ec5378c858f80c9abd5604dcb09d6913
Frank Yeats <louielouiezpc@gmail.com>
2020-02-09 21:36:33 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,21 +1,15 @@
-def count():
-    fs = []
-    for i in range(1, 4):     # 循环变量i
-        def f():
-             return i*i       # 此处引用了循环变量
-        fs.append(f)
-    return fs
+def createCounter():
+    num=[0]
+    def counter():
+        num[0]+=1
+        return num[0]
+    return counter
 
-
-def count():
-    def n(x):
-        def n1():
-            return x*x
-        return n1
-    n2=[]
-    for i in range(1,4):
-        n2.append(n(i)) 
-    return n2
-
-f1, f2, f3 = count()
-print(f1(),f2(),f3())
+# 测试:
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('测试通过!')
+else:
+    print('测试失败!')
