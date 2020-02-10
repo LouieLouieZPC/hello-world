e313e3716a0fc86dac5df04b558b4da72969fc84
Frank Yeats <louielouiezpc@gmail.com>
2020-02-10 14:25:27 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,2 +1,31 @@
-L=list(filter(lambda n: n%2==1,range(1,20)))
-print(L)
+def now():
+    print('2020.2.10')
+f=now     # 函数也是个对象，函数对象被赋值给变量
+f()       # 调用该函数
+
+print(now.__name__)   
+print(f.__name__)    # __name__属性，拿到函数的名字
+
+
+
+f = abs
+print(f)
+
+
+
+from functools import wraps
+def decorator_name(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not can_run:
+            return "Function will not run"
+        return f(*args, **kwargs)
+    return decorated
+ 
+@decorator_name
+def func():
+    return("Function is running")
+
+can_run = False
+print(func())
+# Output: Function will not run
