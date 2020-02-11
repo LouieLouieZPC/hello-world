a89325c1d57bb628104bacb8f290500f9743edf0
Frank Yeats <louielouiezpc@gmail.com>
2020-02-11 19:16:00 +0800

    enter

--- 《Python编程基础》例题练习/test2.py
+++ 《Python编程基础》例题练习/test2.py
@@ -1,16 +1,22 @@
 class Father(object):
     def __init__(self,name):
         self.name=name
-        print('name:%s'%self.name)
+        print('hi')
     def get_name(self):
         return 'Father '+self.name
+
 class Son(Father):
+    def __init__(self, name):
+        self.name=name
+        print('hola')
     def get_name(self):
         return 'Son '+self.name
 
 if __name__ == "__main__":
-    son=Son('Yeats')
+    son=Son('Frank')
     print(son.get_name())
+
+
 
 
 
