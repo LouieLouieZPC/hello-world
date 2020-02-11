d094f1c325485d3de21893383060e3447ee0b50b
Frank Yeats <louielouiezpc@gmail.com>
2020-02-11 14:16:22 +0800

    enter

--- 《Python编程基础》例题练习/4.1函数/steak.py
+++ 《Python编程基础》例题练习/4.1函数/steak.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
+'a test module'  # 表示模块的文档注释，任何模块代码的第一个字符串都被视为模块的文档注释
+
+__auther__='Frank Yeats'  # 使用__author__变量把作者写进去 
+
 def make_steak(d,*other):
     '''做一份牛排'''
     print('Make a steak well done in %d'%d+' with the other:')
