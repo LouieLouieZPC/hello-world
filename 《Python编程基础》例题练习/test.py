0387a0f560029d3f9742393481d04e9083d865ab
Frank Yeats <louielouiezpc@gmail.com>
2020-02-12 15:16:47 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,22 +1,13 @@
-class Father(object):
-    def __init__(self,name):
-        self.name=name
-        print('name:%s'%self.name)
-    def get_name(self):
-        return 'Father '+self.name
-    def test(self):
-        return('这是父类的方法')
+class Myobject(object):
+    def __init__(self):
+        self.x=9
+    def power(self):
+        return self.x
 
-class Son(Father):
-    def get_name(self):
-        return 'Son '+self.name
 
-if __name__ == "__main__":
-    son=Son('Yeats')         # 创建子类对象
-    print(son.test())
-    print(son.get_name())    
-
-# Output:
-name:Yeats       # 子类中没有构造方法，以此默认调用父类的构造方法
-这是父类的方法    # 子类继承且可调用父类的方法
-Son Yeats        # 重装，子类中的方法会覆盖父类中同名的方法
+obj=Myobject()
+print(hasattr(obj,'x'))     # have attribute的简写
+setattr(obj,'y',20)   #set attributed 的简写
+print(obj.y)
+print(getattr(obj,'y'))
+print(obj.z)
