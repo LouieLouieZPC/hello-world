13b82664808931626ca62ab964c40166b6593625
Frank Yeats <louielouiezpc@gmail.com>
2020-02-12 19:43:49 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -3,14 +3,16 @@ from types import MethodType
 class Student(object):
     __slots__=('name','age')
 
+
+
 def set_age(self,age):
     self.age = age
 def set_score(self,score):
     self.score = score
 
-
-
+    
 s=Student()
 s.gender='male'
 print(s.gender)
+
 
