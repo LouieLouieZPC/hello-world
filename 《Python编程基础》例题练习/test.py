c646b23dd228df28fe21c53bead83ddb0254fbe3
Frank Yeats <louielouiezpc@gmail.com>
2020-02-12 15:51:17 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,9 +1,25 @@
 class Student(object):
-    name='Frank'
+    count = 0
 
-s=Student()
-print(s.name)
-print(Student.name)
-s.name=10
-print(s.name)
-print(Student.name)
+    def __init__(self, name):
+        self.name = name
+        Student.count+=1
+
+
+
+
+
+# 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
