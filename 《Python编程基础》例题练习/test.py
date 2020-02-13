0cf6f748c4aa1321735df1fd147d10288ba147bc
Frank Yeats <louielouiezpc@gmail.com>
2020-02-13 14:37:01 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,12 +1,1 @@
-class Student(object):
-    def __init__(self):
-        self.name='frank'
-    def __getattr__(self,attr):
-        if attr=='age':
-            return '21 years'
-        raise AttributeError('\'Student has not attribute %s '%attr)
-
-s=Student()
-print(s.name)
-print(s.age)
-print(s.gender)
+print(callable(mx))
