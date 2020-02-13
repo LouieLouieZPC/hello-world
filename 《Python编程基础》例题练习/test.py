38ce46779b4d571bb046793df0c70de86338076a
Frank Yeats <louielouiezpc@gmail.com>
2020-02-13 22:16:20 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,4 +1,11 @@
-from hello import Hello
-
-h=Hello()
-print(h.hello())
+try:
+    x=int(input('请输入计划代码:'))
+    print('正在执行代码：%s'%x)
+except ValueError:
+    print('您输入的代码有误！')
+else:
+    print('**************************')
+    print('没有异常,正在执行下一段代码')
+finally:
+    print('**************************')
+    print('有无异常都必然执行的代码')
