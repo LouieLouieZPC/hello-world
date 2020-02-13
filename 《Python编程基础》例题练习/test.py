e763bedf31e712d70b981d83c776f80f7ffe194e
Frank Yeats <louielouiezpc@gmail.com>
2020-02-13 13:49:02 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -17,10 +17,8 @@ class Fib(object):
                     L.append(a)
                 a, b = b, a + b
             return L
->>> f = Fib()     # 创建类对象
->>> f[3]
-3
->>> f[0:5]        # 对类对象切片
-[1, 1, 2, 3, 5]
->>> f[:10]
-[1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+frank=Fib()
+print(frank[3])
+print(frank[0:10])
+print(frank[0:10:3])
