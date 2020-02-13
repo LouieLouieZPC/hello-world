c1dad3b1e9dfc5bcdafdc1a9b6437096d00bd396
Frank Yeats <louielouiezpc@gmail.com>
2020-02-13 11:00:57 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,5 +1,9 @@
 class Student(object):
     def __init__(self,name):
         self.name=name
+    def __str__(self):   # 把一个类当作
+        return 'Student object (name:%s)'%self.name
+    __repr__=__str__
 
-print(Student('Yeats'))
+s=Student('Yeats')
+print(s)
