a97969cae6524b4ea9be362e1ab53b608447e5cb
Frank Yeats <louielouiezpc@gmail.com>
2020-02-14 21:50:32 +0800

    enter

---
+++ 《Python编程基础》例题练习/8.1错误、调试和测试/mydict.py
@@ -0,0 +1,13 @@
+class Dict(dict):
+    '''此为测试用例mydict.py代码'''
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
