e5f8081fda1a79c06862d718a6cc92509b2a6a5f
Frank Yeats <louielouiezpc@gmail.com>
2020-02-14 11:05:52 +0800

    enter

--- 《Python编程基础》例题练习/test.py
+++ 《Python编程基础》例题练习/test.py
@@ -1,11 +1,6 @@
 try:
-    x=int(input('请输入计划代码:'))
-    print('正在执行代码：%s'%x)
-except ValueError:
-    print('您输入的代码有误！')
-else:
-    print('**************************')
-    print('没有异常,正在执行下一段代码')
-finally:
-    print('**************************')
-    print('有无异常都必然执行的代码')
+    foo()
+except ValueError as e:
+    print('ValueError')
+except UnicodeError as e:
+    print('UnicodeError')
