dc5b4bda7528af783839977d514629c130d9136d
Frank Yeats <louielouiezpc@gmail.com>
2020-02-15 13:29:46 +0800

    enter

--- 《Python编程基础》例题练习/8.2案例/student.py
+++ 《Python编程基础》例题练习/8.2案例/student.py
@@ -1,15 +1,14 @@
+# -*- coding: utf-8 -*-
+
 class Student(object):
-    def __init__(self,name,score):
-        self.name=name
-        self.score=score
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
     def get_grade(self):
-        if self.score>100:
-            raise ValueError('Value error!')
-        elif self.score>=80 and self.score<100:
+        if self.score > 100 or self.score < 0:
+            raise ValueError('input error!')
+        elif self.score >= 80 and self.score <=100:
             return 'A'
-        elif self.score>=60 and self.score<80:
+        elif self.score >= 60 and self.score <=80:
             return 'B'
-        elif self.score>=0 and self.score<60:
-            return 'C'
-        else:
-            raise ValueError('Value error!')
+        return 'C'
--- 《Python编程基础》例题练习/8.2案例/student_test.py
+++ 《Python编程基础》例题练习/8.2案例/student_test.py
@@ -3,27 +3,32 @@ import unittest
 from student import Student
 
 class TestStudent(unittest.TestCase):
+
     def test_80_to_100(self):
-        s1=Student('Bart',80)
-        s2=Student('Lisa',100)
-        self.assertEqual(s1.get_grade(),'A')
-        self.assertEqual(s1.get_grade(),'A')
+        s1 = Student('Bart', 80)
+        s2 = Student('Lisa', 100)
+        self.assertEqual(s1.get_grade(), 'A')
+        self.assertEqual(s2.get_grade(), 'A')
+
     def test_60_to_80(self):
         s1 = Student('Bart', 60)
         s2 = Student('Lisa', 79)
         self.assertEqual(s1.get_grade(), 'B')
         self.assertEqual(s2.get_grade(), 'B')
+
     def test_0_to_60(self):
         s1 = Student('Bart', 0)
         s2 = Student('Lisa', 59)
         self.assertEqual(s1.get_grade(), 'C')
         self.assertEqual(s2.get_grade(), 'C')
-    def test_invaild(self):
+
+    def test_invalid(self):
         s1 = Student('Bart', -1)
         s2 = Student('Lisa', 101)
-        with self.assertRaises(ValueError):      # 抛出错误
-            s1.get_grade()         # 在这个条件下
+        with self.assertRaises(ValueError):
+            s1.get_grade()
         with self.assertRaises(ValueError):
             s2.get_grade()
-    if __name__ == "__main__":
-        unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()     # 全局的main（）方法。可以将一个单元测试模块变成可以直接运行的测试脚本;main()方法使用testLoader类来搜索所有包含在该模块总以“test”命名开头的测试方法，并自动执行他们
