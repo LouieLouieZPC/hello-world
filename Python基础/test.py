abfac8bff6925e03bbecf4404e3bdfc436d0b58c
Frank Yeats <louielouiezpc@gmail.com>
2020-03-04 19:15:58 +0800

    enter

--- Python基础/test.py
+++ Python基础/test.py
@@ -1,3 +1,3 @@
 import pickle
 d=dict(name='Bob',age=20,score=88)
-pickle.dumps(d) # 用pickle.dumps()方法,把任意对象序列化成一个bytes，然后，就可以把这个bytes写入文件
+print(pickle.dumps(d)) # 用pickle.dumps()方法,把任意对象序列化成一个bytes，然后，就可以把这个bytes写入文件
