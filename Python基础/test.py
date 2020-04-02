771373d5a25ebbc283eeca578b8e7489ab6b637b
Frank Yeats <louielouiezpc@gmail.com>
2020-04-02 23:37:13 +0800

    enter

--- Python基础/test.py
+++ Python基础/test.py
@@ -10,8 +10,7 @@ if __name__ == '__main__':  # 要加这行...
 
     tx = multiprocessing.Queue()  # 创建进程通信的Queue，你可以理解为我拿了个管子来...
     jc = multiprocessing.Process(target=foo, args=(tx,))  # 创建子进程
-    jc.start()  # 启子子进程
-
     print('主进程准备发送数据...')
     tx.put('有内鬼，终止交易！')  # 将管子的一端放在主进程这里，主进程往管子里丢入数据↑
+    jc.start()  # 启子子进程
     jc.join()
