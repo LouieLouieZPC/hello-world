02b47dd87d2f7c46c32513fc284d9f0755daeab0
Frank Yeats <louielouiezpc@gmail.com>
2020-04-04 23:06:57 +0800

    enter

--- Python基础/test.py
+++ Python基础/test.py
@@ -1,29 +1,10 @@
-import time, threading
+import threading, multiprocessing
 
-# 假定这是你的银行存款:
-balance = 0
-lock = threading.Lock()
-def change_it(n):
-    # 先存后取，结果应该为0:
-    global balance
-    balance = balance + n
-    balance = balance - n
+def loop():
+    x = 0
+    while True:
+        x = x ^ 1
 
-def run_thread(n):
-    for i in range(100000):
-        # 先要获取锁:
-        lock.acquire()
-        try:
-            # 放心地改吧:
-            change_it(n)    # 给change_it(n)上好锁了🔒
-        finally:
-            # 改完了一定要释放锁:
-            lock.release()
-
-t1 = threading.Thread(target=run_thread, args=(5,))
-t2 = threading.Thread(target=run_thread, args=(8,))
-t1.start()
-t2.start()
-t1.join()
-t2.join()
-print(balance)
+for i in range(multiprocessing.cpu_count()):
+    t = threading.Thread(target=loop)
+    t.start()
