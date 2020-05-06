4752eb515ac9e3477c422c925a760e74aabe5532
Frank Yeats <louielouiezpc@gmail.com>
2020-05-06 12:46:29 +0800

    enter

--- Python基础/test.py
+++ Python基础/test.py
@@ -1,6 +1,14 @@
-def gen(n):
-  for i in range(n):
-    yield(i**2)
-
-for i in gen(5):
-  print(i,'',end='')
+age=-1
+while age<=0:
+  try:
+    age=int(input('Enter your age in years:'))
+    if age<=0:
+      print('your age must be positive!')
+  except ValueError:
+    print('This is an invaild age specification!')
+  except EOFError:
+    print('There was an unexceted error reading input!')
+    raise
+  finally:
+    print('Your age is:',age)
+  
