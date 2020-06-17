bc79d4bebebe7568b6977bf9996433d849dec40f
e <LouieLouieZPC@gmail.com>
2020-06-18 00:36:00 +0800

    enter

--- Python基础/1.2基础知识小结/输入面积，输出半径及周长.py
+++ Python基础/1.2基础知识小结/输入面积，输出半径及周长.py
@@ -1,8 +1,10 @@
+# 输入圆的半径，输出面积周长
+
 import math                        # 调用数学
 Π=math.pi                          # 定义Π
 Π=int(Π)                           # 对Π取整数
-S=input('输入该圆面积：')
+S=input('输入该圆面积：')           # 输入该圆的面积
 S=float(S)                         # 将字符串转换为浮点数
 r=math.sqrt(S/Π)                   # 用Python数字sqrt()函数开平方
 C=math.sqrt(4*S*Π)
-print('该圆半径为：',r,'该圆周长为：',C)
+print('该圆半径为：',r,'该圆周长为：',C)   #  输出该圆的半径和该圆的周长
